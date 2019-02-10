e9a0549acd9b6b302c494c715f569e96b610639c
Adarsh1193 <35886345+Adarsh1193@users.noreply.github.com>
2019-02-09 19:01:39 -0800

    Add files via upload

---
+++ Variables.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {/*
+            int x;
+            int y;
+
+            x = 7;
+            y = x + 3;
+            Console.WriteLine(y);
+            Console.ReadLine(); */
+
+            Console.WriteLine("What is your first name?");
+            Console.Write("Write your first name");
+            string myfirstName;
+            myfirstName = Console.ReadLine();
+
+            string mysecondName;
+            Console.Write("Write your last name");
+            mysecondName = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + myfirstName + " " + mysecondName);
+            Console.ReadLine();
+        }
+    }
+}
