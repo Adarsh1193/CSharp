63c6facf6b9ab906d05d6b271e8924f91f627c9a
Adarsh1193 <35886345+Adarsh1193@users.noreply.github.com>
2019-02-09 18:58:26 -0800

    Add files via upload

---
+++ Decisions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Desicions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose a number");
+            Console.Write("1, 2 or 3");
+            string chooseValue = Console.ReadLine();
+
+            string message= "";
+
+            if (chooseValue == "1")
+            {
+                message = "You won";
+            }
+            else if (chooseValue == "2")
+            {
+                message = "You lost";
+            }
+            else if (chooseValue == "3")
+            {
+                message = "Draw";
+            }
+            else
+            {
+                message = "Wrong Input";
+            }
+            Console.Write(message);
+            Console.ReadLine();
+        }
+    }
+}
